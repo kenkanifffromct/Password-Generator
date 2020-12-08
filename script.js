f228c372eae30fa0f5d830bb94ac49f1905e5362
Kenneth Keller <kenkanifffromct21@gmail.com>
2020-12-07 20:29:36 -0500

    updating HTML and JS

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateButton = document.querySelector('#generate');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+document.getElementById('generate').onclick = function () {
+  alert("Whazzuuuuup! Are you ready to generate your totally random password?");
+}
 
-  passwordText.value = password;
+
+document.getElementById('generate').addEventListener("click", function() {
+var uppercase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+var uppercaseSplit = uppercase.split("");
+var lowercase = ("abcdefghijklmnopqrstuvwxyz");
+var lowercaseSplit = lowercase.split("");
+var specialCharacters = ("!@#$%^&*()_+<>?:");
+var specialCharactersSplit = specialCharacters.split();
+var numbers = ("123456789");
+var numbersSplit = numbers.split("");
+var showPassword = document.getElementById('password');
+var userCount = prompt("How many characters would you like your password to have? Note: It must be between 8 and 128");
+
+if (userCount < 8 || userCount > 128) {
+    alert("Password must be between 8 and 128 characters.")
+    return
+  }
+
+var numConfirm = confirm("Do you want to include numbers in your password?");
+
+
+
+
+
+
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+)
+
+
+
+
+
+
+
+function pressGenerate()
+ 
+// function characterLength() {
+ 
