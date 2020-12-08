633bec865fa0a95742d1b7a615a918ce455d980d
Kenneth Keller <kenkanifffromct21@gmail.com>
2020-12-07 21:29:00 -0500

    updated JS

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var generateButton = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
+// var displayPassword = document.querySelector("#password");
 
 document.getElementById("generate").onclick = function () {
   alert("Whazzuuuuup! Are you ready to generate your totally random password?");
@@ -14,6 +15,8 @@ document.getElementById("generate").addEventListener("click", function () {
   var numbers = "123456789";
   var numbersSplit = numbers.split("");
   var showPassword = document.getElementById("password");
+  var selectEl = "";
+  var password = "";
   var userCount = prompt(
     "How many characters would you like your password to have? Note: It must be between 8 and 128"
   );
@@ -38,6 +41,61 @@ document.getElementById("generate").addEventListener("click", function () {
   }
 
   if (verify == 0) {
-    alert("You must select at least one character type for your criteria. Nice try though.")
+    alert("You must select at least one character type for your criteria. Nice try though.");
+    return;
   }
 });
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+function generatePWD() {
+    for (var i = 0; i <= length; i++) {
+      password =
+        password +
+        selectEl.charAt(
+          Math.floor(Math.random() * Math.floor(selectEl.length - 1))
+        );
+    }
+  }
+
+  if (lowerConfirm && !upperConfirm && !numberConfirm && !specialConfirm) {
+    selectEl = lowerase;
+    generatePWD();
+  } else if (lowerConfirm && upperConfirm && !numberConfirm && !specialConfirm) {
+    selectEl = lowerase + upperase;
+    generatePWD();
+function generatePWD() {
+      for (var i = 0; i <= length; i++) {
+        password = password + selectEl.charAt(Math.floor(Math.random() * Math.floor(selectEl.length - 1)));
+      }
+    }
+  
+    if (lowerConfirm && !upperConfirm && !numberConfirm && !specialConfirm) {
+      selectEl = lowerase;
+      generatePWD();
+    }
+
+    else if (lowerConfirm && upperConfirm && !numberConfirm && !specialConfirm) {
+      selectEl = lowerase + upperase;
+      generatePWD();
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+// document.getElementById('password').addEventListener("click", writePassword);
